45913e0cc520d26389d06a711ba61e0b6663ca04
lizziedaws <52210773+lizziedaws@users.noreply.github.com>
2019-11-25 20:43:52 -0600

    Add files via upload

---
+++ BusinessList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './BusinessList.css';
+
+const BusinessList = {
+    render() {
+        return (
+        <div className="BusinessList">
+            class Business extends React.Component {}
+            class Business extends React.Component {}
+            class Business extends React.Component {}
+            class Business extends React.Component {}
+            class Business extends React.Component {}
+</div>
+        )
+    }
+}
+export default BusinessList;
---
+++ SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './SearchBar.css';
+
+var sortByOptions = ["Best Match", "Highest Rated", "Most Reviewed"];
+sortByOptions.sort();
+
+class SearchBar extends React.Component() {
+
+};
